0bf80844017c437799e2bcf3819a9c4e0c179739
Вадим Пономарьов <vadimcr17@gmail.com>
2025-09-19 13:01:07 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Vadym";
+        int age = 19;
+        double height = 1.86;
+        boolean IsStudent = false;
+        System.out.println("Name : " + name);
+        System.out.println("Age : " + age);
+        System.out.println("Height : " + height);
+        System.out.println("IsStudent: " +  IsStudent );
+    }
+}
