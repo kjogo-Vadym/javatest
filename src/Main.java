288fcd5d4bda12296b02468f6e0c872af3dabeb3
Вадим Пономарьов <vadimcr17@gmail.com>
2025-09-22 21:47:03 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
         int age = 19;
         double height = 1.86;
         boolean IsStudent = false;
-        System.out.println("Name : " + name);
-        System.out.println("Age : " + age);
-        System.out.println("Height : " + height);
-        System.out.println("IsStudent: " + IsStudent);
+        System.out.println( "Name : " + name );
+        System.out.println( "Age : " + age );
+        System.out.println( "Height : " + height );
+        System.out.println( "Is Student: " + IsStudent );
     }
     }
--- src/ifelse.java
+++ src/ifelse.java
@@ -1,10 +1,10 @@
 public class ifelse {
     public static void main(String[] args) {
         int age = 19;
-        if (age < 18) {
-            System.out.println("Доступ Заборонено");
+        if( age < 18 ) {
+            System.out.println( "Доступ Заборонено" );
         } else {
-            System.out.println("Доступ дозволено");
+            System.out.println( "Доступ дозволено" );
         }
     }
 }
